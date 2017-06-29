3dbe0c9265ff3a6d94840deb23a3baae93d9e85c
millken <millken@gmail.com>
2017-06-29 18:45:18 +0800

    add store

--- proxy/handler.go
+++ proxy/handler.go
@@ -2,9 +2,12 @@ package proxy
 
 import (
 	"crypto/tls"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -13,24 +16,8 @@ type Handler struct {
 }
 
 func (h *Handler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
-	h.getFromOrigin(resp, req)
-}
+	var writer io.Writer
 
-func (h *Handler) modifyRequest(req *http.Request) {
-	aRecord, err := h.queryDNS(req.Host)
-	if err != nil {
-		log.Printf("queryDNS err: %s\n", err)
-	}
-	if req.TLS == nil {
-		req.URL.Scheme = "http"
-	} else {
-		req.URL.Scheme = "https"
-	}
-	req.URL.Host = aRecord
-	req.RequestURI = ""
-}
-
-func (h *Handler) getFromOrigin(resp http.ResponseWriter, req *http.Request) {
 	h.modifyRequest(req)
 	client := &http.Client{
 		Transport: &http.Transport{
@@ -51,7 +38,49 @@ func (h *Handler) getFromOrigin(resp http.ResponseWriter, req *http.Request) {
 		}
 	}
 	defer tres.Body.Close()
-	io.Copy(resp, tres.Body)
+	if tres.StatusCode == 200 {
+		writer = h.archiver(resp, req)
+	} else {
+		writer = resp
+	}
+	io.Copy(writer, tres.Body)
+}
+
+func (h *Handler) archiver(resp http.ResponseWriter, req *http.Request) io.Writer {
+	var writer io.Writer
+	url := fmt.Sprintf("%s%s", req.Host, req.URL.Path)
+	dir, filename := filepath.Split(url)
+	if filename == "" {
+		filename = "index.html"
+	}
+	stat, err := os.Stat(dir)
+	if os.IsNotExist(err) || !stat.IsDir() {
+		if err = os.MkdirAll(dir, 0644); err != nil {
+			log.Printf("can not mkdir : %s, %s\n", dir, err)
+			return resp
+		}
+
+	}
+
+	fhandler, _ := os.Create(fmt.Sprintf("%s%s", dir, filename))
+	log.Printf("url: %s, dir: %s, filename: %s\n", url, dir, filename)
+	writer = io.MultiWriter(resp, fhandler)
+	return writer
+}
+
+func (h *Handler) modifyRequest(req *http.Request) {
+	aRecord, err := h.queryDNS(req.Host)
+	if err != nil {
+		log.Printf("queryDNS err: %s\n", err)
+	}
+	if req.TLS == nil {
+		req.URL.Scheme = "http"
+	} else {
+		req.URL.Scheme = "https"
+	}
+	req.Header.Set("Accept-Encoding", "deflate")
+	req.URL.Host = aRecord
+	req.RequestURI = ""
 }
 
 func NewHandler(ns string) http.Handler {
